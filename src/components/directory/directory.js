e0791b0cf9dad2e0a89d9cedc8e9940ca435acbd
saahil0826 <snawalkha0826@gmail.com>
2021-09-15 21:57:58 +0530

    added directory, menu-item component & navigation through withRouter HOC

---
+++ src/components/directory/directory.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import MenuItem from "../menu-item/menu-item.js";
+import "./directory.scss";
+
+function Directory() {
+  const [sections, setSections] = useState([
+    {
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "hats",
+    },
+    {
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      id: 2,
+      linkUrl: "jackets",
+    },
+    {
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "",
+    },
+    {
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      id: 4,
+      linkUrl: "",
+    },
+    {
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
+      id: 5,
+      linkUrl: "",
+    },
+  ]);
+
+  return (
+    <div className="directory-menu">
+      {sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+        <MenuItem
+          key={id}
+          title={title}
+          imageUrl={imageUrl}
+          size={size}
+          linkUrl={linkUrl}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Directory;
