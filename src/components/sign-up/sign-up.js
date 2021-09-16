926e39ca46fbb5f5f18cee6ceaa6ece12ec99855
saahil0826 <you@example.com>
2021-09-16 21:49:06 +0530

    implemented firebase utils, included ability to store authenticated u… ers into our firestore database

---
+++ src/components/sign-up/sign-up.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import FormInput from "../form-input/form-input.js";
+import CustomButton from "../custom-button/custom-button.js";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import "./sign-up.scss";
+
+export default function SignUp() {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      setDisplayName("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="sign-up">
+      <h2 className="title">I do not have a account</h2>
+      <span>Sign up with your email and password</span>
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          label="Display Name"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          label="Password"
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          label="Confirm Password"
+          required
+        />
+        <CustomButton type="submit">SIGN UP</CustomButton>
+      </form>
+    </div>
+  );
+}
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.js';
+import SignUp from '../../components/sign-up/sign-up.js';
+
 
 import './sign-in-and-sign-up.scss';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
