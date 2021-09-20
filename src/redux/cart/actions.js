16058aa319b36f46de382826664a8ec35d9a5e4e
saahil0826 <snawalkha0826@gmail.com>
2021-09-20 23:00:25 +0530

    updated cart redux files to handle storing our cartItems, added a… …ddCartItem action to collection-item component

--- src/redux/cart/actions.js
+++ src/redux/cart/actions.js
@@ -1,5 +1,10 @@
-import { TOGGLE_CART_HIDDEN } from "./types.js";
+import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./types.js";
 
 export const toggleCartHidden = () => ({
   type: TOGGLE_CART_HIDDEN,
+});
+
+export const addItem = item => ({
+  type: ADD_ITEM,
+  payload: item
 });
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,7 +1,8 @@
-import { TOGGLE_CART_HIDDEN } from "./types.js";
+import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./types.js";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden
       };
+      case ADD_ITEM:
+    return {
+      ...state,
+      cartItems: [...state.cartItems, action.payload]
+    };
     default:
       return state;
   }
